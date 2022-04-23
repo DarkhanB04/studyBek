7bad6fd9eb836dac639a602a1916b827b43814bf
BEKS777 <98129233+BEKS777@users.noreply.github.com>
2022-04-23 13:34:27 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const app = express();
+const https=require('https')
 var bodyParser = require("body-parser")
 const ejs = require('ejs')
 const port = 3000
 app.set('view engine', 'ejs')
 var path = require("path")
 const {Router} = require("express");
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use("/", require("./routes/root"));
 app.use("/teacher", require("./routes/teachers"))
@@ -14,6 +17,8 @@ app.use("/contact", require("./routes/contact"));
 app.use("/about", require("./routes/about"));
 app.use("/assets", express.static(__dirname+"/assets"));
 app.use('/js',express.static(__dirname +'/js'));
+
+
 app.listen(port, () =>
    console.log(`App listening at http://localhost:${port}`)
 
